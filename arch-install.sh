ae8e9693254fb3519c4b71522499facfa7d0d0e8
Charles Eckman <cceckman@gmail.com>
2016-02-10 22:50:33 -0800

    Changes; debugging a stoppage.

--- arch-install.sh
+++ arch-install.sh
@@ -268,9 +268,9 @@ HRD
   fi
   # Make new host keys
   pushd /etc/ssh/
-  rm ssh_host_*key*
-  ssh-keygen -t ed25519 -f ssh_host_ed25519_key < /dev/null
-  ssh-keygen -t rsa -b 4096 -f ssh_host_rsa_key < /dev/null
+    rm -f ssh_host_*key*
+    ssh-keygen -t ed25519 -f ssh_host_ed25519_key < /dev/null
+    ssh-keygen -t rsa -b 4096 -f ssh_host_rsa_key < /dev/null
   popd
   
   systemctl --now enable sshd.service
