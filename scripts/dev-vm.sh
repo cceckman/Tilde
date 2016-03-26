c26384c938fc9c5ebf4f7b56bca36264bbee7cc6
Charles Eckman <charles@cceckman.com>
2016-03-26 22:13:48 +0000

    Add more TODOs for cloud VM mode.

--- scripts/dev-vm.sh
+++ scripts/dev-vm.sh
@@ -8,3 +8,12 @@ exit
 ## https://cloud.google.com/compute/docs/instances/connecting-to-instance
 ## https://github.com/GoogleCloudPlatform/compute-archlinux-image-builder
 ## 
+
+# TODO first: refactor arch-install.sh into a collection of scripts.
+# TODO:
+#  Prompt for image, or make a new one.
+#  Start up VM.
+#  Add local SSH keys to VM / project.
+#  Trigger the appropriate arch-install.sh factors (after refactoring it.)
+#  Add VNC-over-VPN configuration per https://medium.com/google-cloud/linux-gui-on-the-google-cloud-platform-800719ab27c5#.igi2a5x4g
+#   Or, VNC-over-SSH-tunnel, like http://www.cl.cam.ac.uk/research/dtg/attarchive/vnc/sshvnc.html
