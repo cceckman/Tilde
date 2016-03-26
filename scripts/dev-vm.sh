e5919d11edbe018988cfc6b38cd3b5bfefdf2c61
Charles Eckman <cceckman@gmail.com>
2016-03-26 14:53:52 -0700

    Update dev-vm.sh

--- scripts/dev-vm.sh
+++ scripts/dev-vm.sh
@@ -2,3 +2,9 @@
 # A la arch-install.sh: set up a GCloud VM for use as a remote / portable dev machine.
 
 echo "Hi! This script isn't written yet. Sorry!"
+exit
+
+# Some useful links:
+## https://cloud.google.com/compute/docs/instances/connecting-to-instance
+## https://github.com/GoogleCloudPlatform/compute-archlinux-image-builder
+## 
