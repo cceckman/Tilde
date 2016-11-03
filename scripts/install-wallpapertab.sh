84c115e0f70855a10ac608f73640a3185a534711
Charles Eckman <charles@cceckman.com>
2016-11-02 23:41:27 -0700

    Tweak how wallpapers are handled.

---
+++ scripts/install-wallpapertab.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#
+# install-wallpapertab.sh
+# Install crontab for wallpaper rotation.
+
+
+key='wallpaper-rotate'
+export CRONTAB_NOHEADER='N'
+crontab -l \
+  | grep -v "$key" \
+  | cat - \
+    <(echo "* * * * *  DISPLAY=:0.0 feh --bg-fill \"\$(find ~/.wallpaper/ | shuf -n1) # $key \"") \
+  | crontab -
+
