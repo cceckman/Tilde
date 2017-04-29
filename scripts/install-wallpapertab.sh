c77b028d2616c70513fcf972b7aa2444ea3d95a9
Charles Eckman <charles@cceckman.com>
2017-04-29 12:54:35 -0700

    make install-wallpapertab posix-compliant

--- scripts/install-wallpapertab.sh
+++ scripts/install-wallpapertab.sh
@@ -1,13 +1,14 @@
-#! /bin/bash
+#!/bin/sh
 #
 # install-wallpapertab.sh
 # Install crontab for wallpaper rotation.
 
 
 key='wallpaper-rotate'
+f=$(mktemp)
+echo "*/5 * * * *  DISPLAY=:0.0 feh --bg-fill \"\$(find ~/secrets/wallpaper/* | shuf -n1)\" # $key " >$f
 crontab -l \
   | grep -v "$key" \
-  | cat - \
-    <(echo "*/5 * * * *  DISPLAY=:0.0 feh --bg-fill \"\$(find ~/secrets/wallpaper/* | shuf -n1)\" # $key ") \
+  | cat - $f \
   | crontab -
 
