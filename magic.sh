258921be788251e1ea00295623661b39b8c8b53b
Charles Eckman <cceckman@gmail.com>
2017-05-18 21:28:25 -0700

    Small tweak to the ergodox config: allow SW reset

--- magic.sh
+++ magic.sh
@@ -72,6 +72,6 @@ HRD
 # https://wiki.archlinux.org/index.php/Keyboard_configuration_in_Xorg#Frequently_used_XKB_options
 
 # And even more keyboard options from Ergodox:
-# http://configure.ergodox-ez.com/keyboard_layouts/kjeyjd/edit
+# http://configure.ergodox-ez.com/keyboard_layouts/qvdvmj/edit
 # Windows version:
-teensy_loader_cli.exe -mcu=atmega32u4 -w -v ergodox_firmware_kjeyjd_cceckman_0.3.hex
+teensy_loader_cli.exe -mcu=atmega32u4 -w -v ergodox_firmware_foo.hex
