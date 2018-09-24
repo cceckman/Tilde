814e1b7deccd5ac6653948c747984fcb5d00c217
Charles Eckman <cceckman@google.com>
2018-09-24 10:13:33 -0700

    remove more unused scripts

--- scripts/touchpad.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-# From https://wiki.archlinux.org/index.php/Touchpad_Synaptics
-# Toggle touchpad with printscreen
-if which synclient >/dev/null && [ -x $(which synclient) ]
-then
-  synclient TouchpadOff=$(synclient -l | grep -c 'TouchpadOff.*=.*0')
-fi
