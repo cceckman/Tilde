5aef8cbb57be9c9f1a05949d729f4a55cb3debce
Charles Eckman <charles@cceckman.com>
2021-02-28 11:41:31 -0800

    Misc updates

--- rcfiles/prompt.zsh
+++ rcfiles/prompt.zsh
@@ -3,8 +3,16 @@
 #
 . $HOME/rcfiles/repo.sh
 
+if test "$THEME" = "red"
+then
+  _HOSTSTR="%n@%m "
+else
+  _HOSTSTR=""
+fi
+
+
 # Prompt color/look mods...
-PS1="%f%k%F{$THEME}∴ \$? \$(_repo)%1~
+PS1="%f%k%F{$THEME}∴ \$? $_HOSTSTR\$(_repo)%1~
 ∵ %f"
 export PS1
 export CLICOLOR="Yes"
