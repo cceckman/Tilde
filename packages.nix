a71d6b882f36b77565fe04c18698375e22a84dde
Charles Eckman <charles@cceckman.com>
2023-06-25 11:29:54 -0400

    Install Ansible

--- packages.nix
+++ packages.nix
@@ -20,20 +20,21 @@
     pkgs.ffmpeg
   ];
   dev = if ! isDev then [] else [
-    pkgs.cmatrix # TODO: Make an "interactive" set- not quite the same...
+    pkgs.ansible
     pkgs.clang
-    pkgs.go
-    pkgs.python311
-    pkgs.python311Packages.pip
-    pkgs.redo-apenwarr
+    pkgs.cmatrix # TODO: Make an "interactive" set- not quite the same...
     pkgs.gdb
+    pkgs.go
+    pkgs.google-cloud-sdk
     pkgs.graphviz
     pkgs.lld
     pkgs.llvm
-    pkgs.google-cloud-sdk
     pkgs.parted
-    pkgs.sqlite
     pkgs.pv
+    pkgs.python311
+    pkgs.python311Packages.pip
+    pkgs.redo-apenwarr
+    pkgs.sqlite
     # TODO: Make this part of a hermetic build for image-building
     pkgs.libguestfs-with-appliance
     pkgs.guestfs-tools
