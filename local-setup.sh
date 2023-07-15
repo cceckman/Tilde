873c916a023cc5ef78582d717ae030abbbc2f3d4
Charles Eckman <charles@cceckman.com>
2023-07-15 12:05:01 -0400

    Fill out setup scripts

--- local-setup.sh
+++ local-setup.sh
@@ -2,4 +2,44 @@
 
 set -eu
 
-echo >&2 "Running local setup..."
+set -eu
+
+stderr() {
+  echo >&2 "$@"
+}
+
+TILDE="$HOME"/r/github.com/cceckman/Tilde
+
+upgrade_tilde() {
+  (
+    set +x
+    cd "$TILDE"
+    git remote set-url origin git@github.com:cceckman/Tilde.git
+    git fetch --unshallow
+    git submodule update --init --recursive
+    git remote prune origin
+  )
+}
+
+upgrade_links() {
+  (
+    ln -sf "$TILDE"/.config "$HOME"/.config
+    ln -sf "$TILDE"/.gitignore_global "$HOME"/.gitignore_global
+    ln -sf "$TILDE"/.vim "$HOME"/.vim
+    ln -sf "$TILDE"/.vimrc "$HOME"/.vimrc
+    ln -sf "$TILDE"/.zsh "$HOME"/.zsh
+    ln -sf "$TILDE"/rcfiles "$HOME"/rcfiles
+    ln -sf "$TILDE"/scripts "$HOME"/scripts
+    ln -sf "$TILDE"/themes "$HOME"/themes
+
+    mkdir -p .vscode/extensions/
+    ln -sf \
+      "$TILDE"/.vscode/extensions/selenized \
+      "$HOME"/.vscode/extensions/selenized
+  )
+}
+
+main() {
+  upgrade_tilde
+  upgrade_links
+}
--- minimal-setup.sh
+++ minimal-setup.sh
@@ -2,4 +2,45 @@
 
 set -eu
 
-echo >&2 "Running minimal setup..."
+stderr() {
+  echo >&2 "$@"
+}
+
+check_user() {
+  if test "$USER" != "cceckman"
+  then
+    stderr "\$USER is not cceckman; refusing to perform local setup."
+    stderr "(Assuming this is a shared machine.)"
+    exit 1
+  fi
+  stderr "\$USER is cceckman, proceeding."
+}
+
+check_sudo() {
+  stderr "Checking / obtaining sudo permissions..."
+  if ! sudo echo >&2 "Successfully authenticated!"
+  then
+    stderr "Could not perform sudo authentication."
+    stderr "Do you have permission to install onto this host?"
+    exit 1
+  fi
+  stderr "Can use sudo."
+}
+
+main() {
+  check_user
+  check_sudo
+
+  set +x
+  sudo -n apt install -y tmux zsh man
+  ZSH="$(cat /etc/shells | grep zsh | head -1)"
+  sudo -n usermod --shell "$ZSH" "$USER"
+  set -x
+
+  TILDE="$(dirname "$(readlink -f "$0")")"
+  ln -sf "$TILDE"/tmux.conf "$HOME"/.tmux.conf
+  ln -sf "$TILDE"/rcfiles/portablerc.sh "$HOME"/.portablerc.sh
+  ln -sf "$HOME"/.portablerc.sh "$HOME"/.zshrc
+}
+
+main "$@"
