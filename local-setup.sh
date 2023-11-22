0b1cd0659f40513c28e40115661664561d756897
Charles Eckman <charles@cceckman.com>
2023-11-22 14:44:24 -0500

    Add inotify-tools for inotifywait

--- local-setup.sh
+++ local-setup.sh
@@ -155,7 +155,7 @@ install_gcloud() {
 install_tools() {
   # Various tools I like...
   cargo install git-branchless
-  sudo apt-get -y install gh
+  sudo apt-get -y install gh file inotify-tools
 
   install_gcloud
 }
